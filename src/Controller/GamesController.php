c1541ce42d2ccccb42bc9820307aaa494fd8ea8b
Nyle Kelk <nyle.kelk@motivitymobile.com>
2019-11-14 16:25:20 +0000

    Adds Game model and controller

---
+++ src/Controller/GamesController.php
@@ -0,0 +1,14 @@
+<?php
+// src/Controller/GamesController.php
+
+namespace App\Controller;
+
+class GamesController extends AppController
+{
+    public function index()
+    {
+        $this->loadComponent('Paginator');
+        $articles = $this->Paginator->paginate($this->Articles->find());
+        $this->set(compact('articles'));
+    }
+}
