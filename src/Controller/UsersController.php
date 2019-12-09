cc752f65cc78b7b01c603177d0aa469e3cb64652
NYYLE <npkelk@gmail.com>
2019-12-09 22:30:11 +0000

    Latest auth chaNGE

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -65,14 +65,14 @@ class UsersController extends AppController {
 
              if ($users->first() == null) {
                $guid = $this->getGUID();
-               $my_token = Security::hash(Security::randomBytes(32));
-
-               debug();exit;
-               $data = $this->request->getData();
-               $data['devUUID'] = $guid;
-               $data['token'] = $my_token;
 
                $user = $this->Users->patchEntity($user, $this->request->getData());
+               $user->devUUID = $guid;
+               $user->status = 1;
+               $user->email = $this->request->data('email');
+
+               $my_token = Security::hash(Security::randomBytes(32));
+               $user->token = $my_token;
 
                if ($this->Users->save($user)) {
                    $this->Flash->success(__('Registration successful. Your confirmation email has been sent'));
@@ -108,8 +108,6 @@ class UsersController extends AppController {
              'status' => 1
            )
          ));
-
-         debug($token);debug($guid);
 
          $id = $user_find->first()['id'];
          $user = $this->Users->get($id);
